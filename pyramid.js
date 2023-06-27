2c73b673d4ee830e7a7b414c309907435534fb87
WRVR <77961121+GooZhi@users.noreply.github.com>
2023-06-27 16:38:00 +0800

    Update pyramid.js

--- pyramid.js
+++ pyramid.js
@@ -1,10 +1,10 @@
-function printScaledHalfCircle(scaleFactor) {
+function printHalfCircle() {
   let numOfRows = 11;  // 半圆的行数
   let charToUse = "a";  // 用于组成半圆的字符
 
   for (let row = 0; row < numOfRows; row++) {
-    let numOfChars = Math.round(2 * Math.sqrt(row * (numOfRows - row)) * scaleFactor); // 根据行号计算当前行需要输出字符的个数，并乘以比例因子
-    let numOfSpaces = Math.round((numOfRows - numOfChars) / 2 * scaleFactor);  // 根据当前行需要输出字符的个数和比例因子计算当前行需要输出空格的个数
+    let numOfChars = 2 * row + 1;  // 当前行需要输出字符的个数
+    let numOfSpaces = numOfRows - row - 1;  // 当前行需要输出空格的个数
 
     // 输出空格
     for (let i = 0; i < numOfSpaces; i++) {
